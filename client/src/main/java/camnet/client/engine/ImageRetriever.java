da7835738ab50786a6ee2abdba4e7c5ec14a3af7
Christopher Blunck <chris@thebluncks.com>
2017-04-06 00:37:49 -0400

    implemented retrieveImage

--- client/src/main/java/camnet/client/engine/ImageRetriever.java
+++ client/src/main/java/camnet/client/engine/ImageRetriever.java
@@ -4,12 +4,50 @@ import camnet.client.model.internal.Camera;
 
 import org.springframework.stereotype.Component;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.MalformedURLException;
+
+
 
 @Component
 public class ImageRetriever {
 
 	public byte[] retrieveImage(Camera camera) throws ImageRetrievalException {
-		return new byte[0];
+		URL url;
+		try {
+			url = new URL(camera.getUrl());
+		} catch (MalformedURLException e) {
+			throw new ImageRetrievalException("malformed url", e);
+		}
+
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		InputStream is = null;
+		try {
+  			is = url.openStream ();
+  			byte[] byteChunk = new byte[4096]; // Or whatever size you want to read in at a time.
+  			int n;
+
+  			while ( (n = is.read(byteChunk)) > 0 ) {
+    			baos.write(byteChunk, 0, n);
+  			}
+		}
+		catch (IOException e) {
+  			System.err.printf ("Failed while reading bytes from %s: %s", url.toExternalForm(), e.getMessage());
+  			e.printStackTrace ();
+		}
+		finally {
+  			if (is != null) {
+  				try {
+  					is.close(); 
+  				} catch (IOException e) {
+  					System.err.println("failed to close stream");
+  				}
+  			}
+		}
+		return baos.toByteArray();
 	}
 
 }
