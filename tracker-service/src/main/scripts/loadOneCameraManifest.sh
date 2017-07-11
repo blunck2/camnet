602f78e9700e82d60e5b86d32d5cfd366051c1ad
Christopher Blunck <chris@thebluncks.com>
2017-07-10 22:24:35 -0400

    use the proper directory

--- tracker-service/src/main/scripts/loadOneCameraManifest.sh
+++ tracker-service/src/main/scripts/loadOneCameraManifest.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -x
 
-CONFIG_FILE=../../../../allCamerasManifest.json
+CONFIG_FILE=../resources/config/oneCameraManifest.json
 
 curl -vv -X POST -u admin:admin --data @${CONFIG_FILE} --header "Content-Type:application/json" http://localhost:8383/api/tracker/manifest/cameras
