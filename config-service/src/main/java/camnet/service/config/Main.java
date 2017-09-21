83d1a19ec0788cb7f89fcc0ca947c44281f7c152
Christopher Blunck <chris@thebluncks.com>
2017-09-21 01:20:40 -0400

    permit different log levels based on appender

--- config-service/src/main/java/camnet/service/config/Main.java
+++ config-service/src/main/java/camnet/service/config/Main.java
@@ -3,11 +3,19 @@ package camnet.service.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @SpringBootApplication
 public class Main {
 
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+
     public static void main(String[] args) {
+        logger.trace("starting application...");
         SpringApplication.run(Main.class, args);
+        logger.trace("application started");
     }
 
 }
