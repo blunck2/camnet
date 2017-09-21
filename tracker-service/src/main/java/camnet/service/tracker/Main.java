60adbcce8d94713f2575b3026ceba1058fb19edb
Christopher Blunck <chris@thebluncks.com>
2017-09-21 01:46:22 -0400

    separate logging to console vs to logfile.  allow us to log to file at TRACE while logging to console at INFO

--- tracker-service/src/main/java/camnet/service/tracker/Main.java
+++ tracker-service/src/main/java/camnet/service/tracker/Main.java
@@ -2,12 +2,18 @@ package camnet.service.tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class Main {
 
+    private final static Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
+        logger.trace("starting application...");
         SpringApplication.run(Main.class, args);
+        logger.trace("application started.");
     }
 
 }
